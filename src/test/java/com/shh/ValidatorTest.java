d3a2fd8414cc662bbbcd977880498e7329380a86
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2026-03-27 17:12:58 +0300

    add validator test

--- src/test/java/com/shh/ValidatorTest.java
+++ src/test/java/com/shh/ValidatorTest.java
@@ -1,6 +1,6 @@
 package com.shh;
 
-import com.shh.model.Command;
+
 import com.shh.model.CommandType;
 import org.junit.jupiter.api.Test;
 
@@ -11,42 +11,102 @@ public class ValidatorTest {
     private Validator validator = new Validator();
 
     @Test
-    public void deleteIsOkTest() {
+    public void createTest() {
         // given
-        String [] data = {"DELETE 5"};
+        String [] data = {"Create", "Hello"};
 
+        // when
+        CommandType result = validator.validate(data);
+
+        // then
         assertDoesNotThrow(() -> validator.validate(data));
+        assertEquals(CommandType.CREATE, result);
+    }
+
+    @Test
+    public void getTest() {
+        // given
+        String [] data = {"Get", "5"};
+
+        // when
+        CommandType result = validator.validate(data);
+
+        // then
+        assertDoesNotThrow(() -> validator.validate(data));
+        assertEquals(CommandType.GET, result);
+    }
+
+    @Test
+    public void getAllTest() {
+        // given
+        String [] data = {"Get"};
+
+        // when
+        CommandType result = validator.validate(data);
+
+        // then
+        assertDoesNotThrow(() -> validator.validate(data));
+        assertEquals(CommandType.GET, result);
+    }
+
+    @Test
+    public void updateTest() {
+        // given
+        String [] data = {"Update", "5", "Hello"};
+
+        // when
+        CommandType result = validator.validate(data);
+
+        // then
+        assertDoesNotThrow(() -> validator.validate(data));
+        assertEquals(CommandType.UPDATE, result);
+    }
+
+    @Test
+    public void deleteTest() {
+        // given
+        String [] data = {"DELETE", "5"};
+
+        // when
+        CommandType result = validator.validate(data);
+
+        // then
+        assertDoesNotThrow(() -> validator.validate(data));
+        assertEquals(CommandType.DELETE, result);
 
     }
 
     @Test
     public void deleteFails1Test() {
         // given
-        String [] data = {"DELETE 5 abc"};
+        String [] data = {"DELETE", "5", "abc"};
 
+        // when
         Exception ex = assertThrows(
                 IllegalArgumentException.class,
                 () -> validator.validate(data)
         );
 
-        assertEquals("abc", ex.getMessage());
+        // then
+        assertEquals("Invalid number of arguments", ex.getMessage());
 
     }
 
     @Test
     public void deleteFails2Test() {
-
+        // given
         String [] data = {"DELETE","asd"};
 
+        // when
         Exception ex = assertThrows(
                 IllegalArgumentException.class,
                 () -> validator.validate(data)
         );
 
-        assertEquals("wrong id", ex.getMessage());
+        // then
+        assertEquals("ID must be a number", ex.getMessage());
 
     }
-
 
 
 }
