9cb891d7038d15fad4822c1a2bba6527a9b9257a
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2026-03-30 10:18:56 +0300

    update some code in main class

--- src/main/java/com/shh/Main.java
+++ src/main/java/com/shh/Main.java
@@ -1,39 +1,32 @@
 package com.shh;
 
+import com.shh.config.ConfigApp;
 import com.shh.handler.CommandDispatcher;
-import com.shh.handler.CreateHandler;
-import com.shh.handler.GetAllHandler;
 import com.shh.model.Command;
 import com.shh.model.OutputMessage;
-import com.shh.repository.Storage;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Storage storage = new Storage();
-        CreateHandler createHandler = new CreateHandler(storage);
-        GetAllHandler getAllHandler = new GetAllHandler(storage);
-        CommandDispatcher commandDispatcher = new CommandDispatcher(createHandler, null, getAllHandler, null, null);
+        ConfigApp configApp = new ConfigApp();
+        CommandDispatcher dispatcher = configApp.dispatcher();
+        Parser parser = configApp.parser();
 
         Scanner scanner = new Scanner(System.in);
-        Validator validator = new Validator();
-        Parser parser = new Parser(validator);
-
 
 
         while (true){
             try {
                 var inputLine = scanner.nextLine();
 
-
                 if (inputLine.equals("exit"))  {
                     break;
                 }
 
                 Command command = parser.parse(inputLine);
-                OutputMessage result = commandDispatcher.handleCommand(command);
+                OutputMessage result = dispatcher.handleCommand(command);
 
                 System.out.println(result.getMsg());
 
