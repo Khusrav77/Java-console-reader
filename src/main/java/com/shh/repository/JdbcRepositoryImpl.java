62825a556f1b30258a543d8f49c740c5bed88e45
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2026-04-23 11:27:15 +0300

    finished: JdbcRepositoryImpl

--- src/main/java/com/shh/repository/JdbcRepositoryImpl.java
+++ src/main/java/com/shh/repository/JdbcRepositoryImpl.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
@@ -14,18 +15,22 @@ public class JdbcRepositoryImpl implements JdbcRepository<Person>{
 
     private final Connection connection;
 
+    private static String INSERT = "INSERT INTO person (name, age) VALUES (?, ?)";
+    private static String SELECT = "SELECT * FROM person WHERE id = ?";
+    private static String SELECT_ALL = "SELECT * FROM person";
+    private static String UPDATE = "UPDATE person SET name = ?, age = ? WHERE id = ?";
+    private static String DELETE = "DELETE FROM person WHERE id = ?";
+
     public JdbcRepositoryImpl(Connection connection) {
         this.connection = connection;
     }
 
     @Override
     public Integer create(Person person) {
-        String sql = "INSERT INTO person (name, age) VALUES (?, ?)";
 
-        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+        try (PreparedStatement ps = connection.prepareStatement(INSERT)) {
             ps.setString(1, person.getName());
             ps.setInt(2, person.getAge());
-
             ps.executeUpdate();
 
             ResultSet rs = ps.getGeneratedKeys();
@@ -41,21 +46,68 @@ public class JdbcRepositoryImpl implements JdbcRepository<Person>{
 
     @Override
     public Optional<Person> get(Integer id) {
+        try(PreparedStatement ps = connection.prepareStatement(SELECT)) {
+            ps.setInt(1, id);
+
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return Optional.of(new Person(
+                rs.getInt("id"),
+                rs.getString("name"),
+                rs.getInt("age")));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return Optional.empty();
     }
 
     @Override
     public Collection<Person> getAll() {
-        return List.of();
+        List<Person> list = new ArrayList<>();
+
+        try(PreparedStatement ps = connection.prepareStatement(SELECT_ALL)) {
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Person(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getInt("age")
+                ));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 
     @Override
     public void update(Integer id, Person person) {
 
+        try(PreparedStatement ps = connection.prepareStatement(UPDATE)) {
+            ps.setString(1, person.getName());
+            ps.setInt(2, person.getAge());
+            ps.setInt(3, id);
+
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     public boolean delete(Integer id) {
+        try(PreparedStatement ps = connection.prepareStatement(DELETE)) {
+            ps.setInt(1, id);
+           return ps.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 }
